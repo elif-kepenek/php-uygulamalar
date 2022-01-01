91300396d09d8f0a287b5cde0d30fb7fe8296781
elif-kepenek <elif.kepenek.98@gmail.com>
2022-01-01 14:26:11 +0300

    sayfa duzenlendi

--- form/index.php
+++ form/index.php
@@ -1,13 +1,3 @@
-<!-- Kullanıcıdan sayı değeri alabileceğiniz bir form hazırlayın.
-Gelen sayı değerlerinin 3 ile bölümünden kalanın 0 olup olmadığını kontrol eden bir fonksiyon yazın.
-Kullanıcıya girilen değerin 3 bölünebilirliğini, bölünemiyorsa bölünebilen ilk değeri kullanıcıya bildirin.
-Boş değer gönderilirse değerin boş olmayacağını bildirin.
-
-
-Örnek:
-
-Girilen Sayı : 4
-Girdiğiniz sayı 3'e bölünemez. Bölünebilecek ilk sayı 6'dır. -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
