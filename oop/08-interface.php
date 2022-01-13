ec597b8219830c95d816e0d9a6ff44b63cda3ec7
elif-kepenek <elif.kepenek.98@gmail.com>
2022-01-13 13:06:18 +0300

    Object Oriented Programming

--- oop/08-interface.php
+++ oop/08-interface.php
@@ -1,15 +1,15 @@
 <?php
 
 interface A{
-    // public function a(){}
-    // public function b(){}
+    public function a();
+    public function b();
 }
 
 // interface başka bir interface'den kalıtım yoluyla kullanılabilir.
 
 interface B{
-    // public function c(){}
-    // public function d(){}
+    public function c();
+    public function d();
 }
 
 // abstract sınıflarda sadece bir tane sınıftan kalıtım yoluyla alabiliyoruz interface ile birden fazla sınıftan kalıtım yoluyla ulaşabiliriz.
